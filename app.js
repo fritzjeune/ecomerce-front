6aa8fe3f30b3521521371396ceb480721cc1f47e
fritz jeune <fritzjeune@outlook.com>
2020-09-12 19:29:18 -0400

    configured ejs

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const express = require('express');
 
 const app = express()
 
+app.set("views", './views')
 app.set("view engine", "ejs")
+
+
+app.get('/', (req, res, next)=> {
+    res.render('home');
+})
 
 
 app.listen(3005, ()=> {
